13bd2dcd64240231b9f7c6093b17bbf448a497da
Celina Takeda <82118386+celinatakeda@users.noreply.github.com>
2022-10-09 10:50:52 -0300

    commit

--- backend/teacher/models.py
+++ backend/teacher/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 class Professor(models.Model):
     nome = models.CharField(max_length=100, null=False, blank=False)
     valor_hora = models.DecimalField(
@@ -8,3 +7,14 @@ class Professor(models.Model):
     )
     descricao = models.TextField(null=False, blank=False)
     foto = models.URLField(max_length=255, null=False, blank=False)
+
+class Aula(models.Model):
+    nome = models.CharField(max_length=100, null=False, blank=False)
+    email = models.EmailField(max_length=255, null=False, blank=False)
+    professor = models.ForeignKey(
+        to=Professor, 
+        on_delete=models.CASCADE, 
+        related_name="aulas", 
+        null=False, 
+        blank=False
+    )
--- backend/teacher/serializers.py
+++ backend/teacher/serializers.py
@@ -1,9 +1,18 @@
 from rest_framework import serializers
 
-from teacher.models import Professor
+from teacher.models import Professor, Aula
 
 class ProfessorSerializer(serializers.ModelSerializer):
   class Meta:
     model = Professor
     fields = '__all__' # pega todos os atributos 
     #fields = ('nome', 'valor_hora', 'descricao', 'foto')
+
+class CadastrarAulaSerializer(serializers.Serializer):
+  email = serializers.EmailField(max_length=255)
+  nome = serializers.CharField(max_length=100)
+
+class AulaSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Aula
+    fields = '__all__'
