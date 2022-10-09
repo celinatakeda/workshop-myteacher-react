6372e1e949506e19a341beb49b34d46cd8e4b111
Celina Takeda <82118386+celinatakeda@users.noreply.github.com>
2022-10-09 11:16:37 -0300

    commit

--- backend/teacher/views.py
+++ backend/teacher/views.py
@@ -29,4 +29,10 @@ class CadastrarAulaAPIView(APIView):
       aula.save()
       aula_serializer = AulaSerializer(aula, many=False)
       return Response(aula_serializer.data, status=HTTP_201_CREATED)
-    return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+    return Response(
+      {
+        "message": "Houveram erros de validação",
+        "errors": serializer.errors
+      },
+      status=HTTP_400_BAD_REQUEST
+    )
