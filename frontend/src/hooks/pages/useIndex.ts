43eed7c62b07b9c4b2dc08a54d2e22c3af46e0ea
Celina Takeda <82118386+celinatakeda@users.noreply.github.com>
2022-10-11 10:27:16 -0300

    Update useIndex.ts

--- frontend/src/hooks/pages/useIndex.ts
+++ frontend/src/hooks/pages/useIndex.ts
@@ -1,37 +1,17 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Professor } from '../../@types/professor';
+import { ApiService } from "../../services/ApiService";
 
 export function useIndex() {
   const [listaProfesssores, setLisraProfessores] = useState<Professor[]>([
-    {
-      id: 1,
-      nome: "Professor 1",
-      foto: "https://github.com/elton-fonseca.png",
-      descricao: "Descrição do professor 1",
-      valor_hora: 100
-    },
-    {
-      id: 2,
-      nome: "Professor 2",
-      foto: "https://github.com/elton-fonseca.png",
-      descricao: "Descrição do professor 1",
-      valor_hora: 200
-    },
-    {
-      id: 3,
-      nome: "Professor 3",
-      foto: "https://github.com/elton-fonseca.png",
-      descricao: "Descrição do professor 1",
-      valor_hora: 300
-    },
-    {
-      id: 4,
-      nome: "Professor 4",
-      foto: "https://github.com/elton-fonseca.png",
-      descricao: "Descrição do professor 1",
-      valor_hora: 400
-    }
+    
   ]);
+
+  useEffect(() => {
+    ApiService.get('/professores').then((resposta) => {
+      setLisraProfessores(resposta.data);
+    })
+  }, [])
 
   return {
     listaProfesssores
